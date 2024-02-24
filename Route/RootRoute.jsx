54faddc83a9bc77064fe1f10d41bac908f3c8b8a
Infinite-Null <ankit.kum.sha9933@gmail.com>
2024-02-25 00:54:10 +0530

    Done with animation bottom navigation

--- Route/RootRoute.jsx
+++ Route/RootRoute.jsx
@@ -5,19 +5,19 @@ import { LibraryRoute } from "./Library/LibraryRoute";
 import Entypo from "react-native-vector-icons/Entypo";
 import Octicons from "react-native-vector-icons/Octicons";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { View } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
 const Tab = createBottomTabNavigator();
 export const RootRoute = () => {
   return (
     <>
       <Tab.Navigator  screenOptions={{tabBarLabelStyle:{color:"white"},headerShown:false, tabBarStyle: {
-          backgroundColor:"rgb(25,25,24)",
+          backgroundColor:"rgb(28,27,28)",
           borderColor:"rgb(22,22,24)",
           elevation:10}}}>
         <Tab.Screen options={{
           // eslint-disable-next-line react/no-unstable-nested-components
           tabBarIcon: ({ color, size, focused }) => (
-            focused ? <View style={{
+            focused ? <Animated.View key={Math.random() + Math.random() + Math.random()} entering={FadeInDown.duration(200)} style={{
               backgroundColor:"rgb(222,222,222)",
               height:size + 15,
               width:size + 15,
@@ -26,13 +26,13 @@ export const RootRoute = () => {
               borderRadius:10,
               marginBottom:10,
               elevation:4,
-            }}><Octicons name="home" color={"black"} size={size - 5} /></View> : <Octicons name="home" color={color} size={size-2} />
+            }}><Octicons name="home" color={"black"} size={size - 5} /></Animated.View> : <Octicons name="home" color={color} size={size-2} />
           ),
         }} name="Home" component={HomeRoute} />
         <Tab.Screen options={{
           // eslint-disable-next-line react/no-unstable-nested-components
           tabBarIcon: ({ color, size, focused }) => (
-            focused ? <View style={{
+            focused ? <Animated.View key={Math.random() + Math.random() + Math.random()} entering={FadeInDown.duration(200)} style={{
               backgroundColor:"rgb(222,222,222)",
               height:size + 15,
               width:size + 15,
@@ -41,13 +41,13 @@ export const RootRoute = () => {
               borderRadius:10,
               marginBottom:10,
               elevation:4,
-            }}><Entypo name="compass" color={"black"} size={size - 5} /></View> : <Entypo name="compass" color={color} size={size - 2} />
+            }}><Entypo name="compass" color={"black"} size={size - 5} /></Animated.View> : <Entypo name="compass" color={color} size={size - 2} />
           ),
         }} name="Discover" component={DiscoverRoute} />
         <Tab.Screen options={{
           // eslint-disable-next-line react/no-unstable-nested-components
           tabBarIcon: ({ color, size, focused }) => (
-            focused ? <View style={{
+            focused ? <Animated.View key={Math.random() + Math.random() + Math.random()} entering={FadeInDown.duration(200)} style={{
               backgroundColor:"rgb(222,222,222)",
               height:size + 15,
               width:size + 15,
@@ -56,7 +56,7 @@ export const RootRoute = () => {
               borderRadius:10,
               marginBottom:10,
               elevation:4,
-            }}><MaterialCommunityIcons name="music-box-multiple-outline" color={"black"} size={size - 5} /></View> : <MaterialCommunityIcons name="music-box-multiple-outline" color={color} size={size - 2} />
+            }}><MaterialCommunityIcons name="music-box-multiple-outline" color={"black"} size={size - 5} /></Animated.View> : <MaterialCommunityIcons name="music-box-multiple-outline" color={color} size={size - 2} />
           ),
         }}  name="Library" component={LibraryRoute} />
       </Tab.Navigator>
