8a34b311c46041bf5a3106c803127c1605d9b834
Infinite-Null <ankit.kum.sha9933@gmail.com>
2024-05-22 18:00:18 +0530

    push

--- Api/YoutubeMusic/Song.js
+++ Api/YoutubeMusic/Song.js
@@ -1,6 +1,7 @@
 import YTMusic from "ytmusic-api";
 import axios from "axios";
 import {baseUrl} from "./baseUrl";
+import {downloadManager} from 'ytmusic_api_unofficial'
 async function searchYoutubeMusicSong(query){
   const ytmusic = new YTMusic()
   await ytmusic.initialize(/* Optional: Custom cookies */)
@@ -13,13 +14,16 @@ async function getYoutubeMusicStreamUrl(id){
     url: baseUrl + "/getDownloadUrl/" + id,
     headers: { },
   };
-  try {
-    const response = await axios.request(config);
-    return response.data
-  }
-  catch (error) {
-    throw error
-  }
+  const result  = await downloadManager.download(id, 'mp3', "high")
+  return result
+  // try {
+  //   const response = await axios.request(config);
+  //   console.log(response.data);
+  //   return response.data
+  // }
+  // catch (error) {
+  //   throw error
+  // }
 }
 async function getYoutubeMusicSuggestion(id){
   let config = {
--- Api/YoutubeMusic/baseUrl.js
+++ Api/YoutubeMusic/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = "https://melody-youtube-music.vercel.app/api"
+export const baseUrl = "http://localhost:3000/api"
